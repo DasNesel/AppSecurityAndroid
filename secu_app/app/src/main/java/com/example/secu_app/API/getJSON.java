3b405f4dffb267e87afba5f71c1aab451bf33f8a
Nesel <matthieu0608@hotmail.fr>
2021-02-03 16:12:21 +0100

    Modification traitement API
    Restriction des droits pour le fichier contenant l'URL de l'API
    Essaie de mise en place de la lecture du fichier pour récupérer l'URL

--- secu_app/app/src/main/java/com/example/secu_app/API/getJSON.java
+++ secu_app/app/src/main/java/com/example/secu_app/API/getJSON.java
@@ -1,21 +1,62 @@
 package com.example.secu_app.API;
 
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class getJSON {
 
-    public static final String BASE_URL = "https://6007f1a4309f8b0017ee5022.mockapi.io/api/m1/";
     public static Retrofit retrofit;
+    private String baseUrl;
+    private String fileName = "APIURL";
 
-    public static Retrofit getInstance() {
-        if (retrofit == null) {
+    public Retrofit getInstance() {
+        if (retrofit == null && this.baseUrl != null || this.baseUrl != "FileNotFoundException") {
             retrofit = new retrofit2.Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(this.baseUrl)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
         return retrofit;
     }
     //public GetEvents create(Class<GetEvents> retrofitInterfaceClass) { return null; }
+/*
+    private void setBaseUrl(){
+        if (fileName != null) {
+            try {
+                //open the file and retrieve the inputStream
+                InputStream inputStream = openFileInput(fileName);
+                if (inputStream != null) {
+                    // open a reader on the inputStream
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                    // String to use to store read lines
+                    String str;
+                    StringBuilder buf = new StringBuilder();
+
+                    // Read the file
+                    while ((str = reader.readLine()) != null) {
+                        buf.append(str + "\r\n");
+                    }
+                    // Close the reader
+                    reader.close();
+                    // Close the inputStream
+                    inputStream.close();
+                    // Affect the text to the textView
+                    textViewFile.setText(buf.toString());
+                }
+            } catch (java.io.FileNotFoundException e) {
+                Toast.makeText(this, "FileNotFoundException", Toast.LENGTH_LONG);
+            } catch (IOException e) {
+                Toast.makeText(this, "FileNotFoundException", Toast.LENGTH_LONG);
+            }
+        }
+    }
+*/
 }
